43023dfe9030df4908e3171ecb2478350f9a231b
Tika26 <shivantikas@yahoo.com>
2023-12-12 22:00:30 -0500

    update

--- crowdfunding_schema_db.sql
+++ crowdfunding_schema_db.sql
@@ -1,24 +1,8 @@
-CREATE TABLE "category" (
-    "category_id" VARCHAR(255)   NOT NULL,
-    "category" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
-);
-
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(255)   NOT NULL,
-    "subcategory" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
-);
-
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR(255)   NOT NULL,
-    "description" VARCHAR(255)   NOT NULL,
+    "Description" VARCHAR(255)   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
     "outcome" VARCHAR(255)   NOT NULL,
@@ -42,6 +26,22 @@ CREATE TABLE "contacts" (
     "email" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
      )
 );
 
